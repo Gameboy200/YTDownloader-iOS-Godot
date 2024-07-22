1ab54fb05d6baa89b49ef7c6a60798fdab6fb02a
“Gameboy200” <maxmcgrath2011@gmail.com>
2024-07-22 09:56:22 +0100

    did some stuff

--- UI.gd
+++ UI.gd
@@ -1,8 +1,14 @@
 extends Node2D
 
+var timer
+var loaded
+var serstatus
+var deployid
 var inputted = false
 var button_press = false
 var current_call_id = 0
+var theurl = "https://ytdownloader-flask.onrender.com"
+#var theurl = "http://192.168.0.95:5000"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,9 +23,15 @@ func _on_LineEdit_text_entered(url1):  # Adjust the path if necessary
 	var line_edit_node = get_node("Panel/LineEdit")
 	var url = url1
 	check_progress(url)
+	timer = Timer.new()
+	add_child(timer)
+	timer.connect("timeout", self, "_on_timeout")
+	timer.wait_time = 10.0  # 10 seconds 
+	timer.one_shot = true  # Make sure it only triggers once
+	timer.start()
 
 func check_progress(url):
-	var endpoint = "http://192.168.0.95:5000/parce"  # Replace with your server address
+	var endpoint = theurl + "/parce"  # Replace with your server address
 	var headers = ["Content-Type: text/plain"]  # Use text/plain for raw text data
 	var request_body = url  # Sending raw text
 
@@ -33,15 +45,77 @@ func check_progress(url):
 		print("Error: Unable to send request")
 		
 
- # Replace with your URL
- # Replace with desired output path
+var API_KEY = 'rnd_dLAoUEoGSJazQec0IRrG113Pz40S'
+var SERVICE_ID = 'srv-cqe2409u0jms7393mv80'
+
+# Function to continuously check the deploy status
+func monitor_deploy():
+	print(11)
+	var http_request = HTTPRequest.new()
+	add_child(http_request)
+	http_request.connect("request_completed", self, "_on_start_completed")
+	var url = "https://api.render.com/deploy/srv-cqe2409u0jms7393mv80?key=ALXc-yHzBlc"  # Replace with your URL
+	var result = http_request.request(url)
+	if result != OK:
+		print("Request failed with error code: %d" % result)
+
+func _on_start_completed(result, response_code, headers, body):
+	if response_code == 200:
+		var bos = body.get_string_from_utf8()
+		var jos = JSON.parse(bos)
+		print("Response: %s" % jos.result['deploy']['id'])
+		deployid = jos.result['deploy']['id']
+		yield(sleep(6.0), "completed")
+		monitor_check()
+	else:
+		print("Request failed with response code: %d" % response_code)
+
+func monitor_check():
+	var http_request = HTTPRequest.new()
+	add_child(http_request)
+	http_request.connect("request_completed", self, "_on_check_completed")
+	var url = "https://api.render.com/v1/services/srv-cqe2409u0jms7393mv80/deploys/" + deployid  # Replace with your URL
+	var headers = ['accept: application/json', 'authorization: Bearer rnd_dLAoUEoGSJazQec0IRrG113Pz40S']
+	var result = http_request.request(url, headers)
+	if result != OK:
+		print("Request failed with error code: %d" % result)
+	return serstatus
+		
+func _on_check_completed(result, response_code, headers, body):
+	if response_code == 200:
+		var bos = body.get_string_from_utf8()
+		var jos = JSON.parse(bos)
+		#print("Response: %s" % jos.result['status'])
+		serstatus = "%s" % jos.result['status']
+	else:
+		print("Request failed with response code: %d" % response_code)
+		
+func startncheck():
+	monitor_deploy()
+	yield(sleep(10.0), "completed")
+	while true:
+		var status = monitor_check()
+		var pro = $Panel3
+		var progresss = $Panel3/Label
+		pro.visible = true
+		if status:
+			print('Deploy status:' + status)
+			progresss.text = 'Starting server'
+			if status == 'live':
+				progresss.text = 'Uploading'
+				pro.visible = false
+				break
+		else:
+			print('Error fetching status. Exiting.')
+			break
+		yield(sleep(1.0), "completed")
 
 func progress():
 	var http_reques = HTTPRequest.new()
 	var progre = $Panel3
 	add_child(http_reques)
 	http_reques.connect("request_completed", self, "_on_request_got")
-	http_reques.request("http://192.168.0.95:5000/progress")
+	http_reques.request(theurl + "/progress")
 	progre.visible = true
 
 func _on_request_got(result: int, response_code: int, headers: Array, body: PoolByteArray):
@@ -56,7 +130,7 @@ func _on_request_got(result: int, response_code: int, headers: Array, body: Pool
 		print(stage)
 		progret.text = stage
 		if not stage == 'Sending':
-			yield(sleep(1.0), "completed")
+			yield(sleep(5.0), "completed")
 			progress()
 		else:
 			progre.visible = false
@@ -89,7 +163,7 @@ func download_mp4(url, ytt):
 			# Create the POST request
 			var header = ["Content-Type: text/plain"]
 			var body = url  # Add your POST data here if needed
-			var url2 = "http://192.168.0.95:5000/download"
+			var url2 = theurl +  "/download"
 			# Send the request
 			var error = http_request.request(url2, header, false, HTTPClient.METHOD_POST, body)
 	
@@ -97,7 +171,7 @@ func download_mp4(url, ytt):
 				print("Failed to send request.")
 			else:
 				print("Request sent, waiting for response...")
-				yield(sleep(2.0), "completed")
+				#yield(sleep(20.0), "completed")
 				progress()
 
 func _on_d_request_completed(result, response_code, headers, body):
@@ -108,7 +182,7 @@ func _on_d_request_completed(result, response_code, headers, body):
 		var response_dict = json_result.result
 		var stage = response_dict.get("file", "Unknown")
 		print(stage)
-		var filem = "http://192.168.0.95:5000/files/" + stage
+		var filem = theurl + "/files/" + stage
 		print(filem)
 		OS.clipboard = filem
 		OS.alert('Download url copyed to clipboard, paste it in safari to download it')
@@ -176,10 +250,13 @@ func sleep(duration):
 
 func _on_request_completed(result, response_code, headers, body):
 	if response_code == 200:
+		if timer.is_stopped() == false:
+			timer.stop()
 		var body_string = body.get_string_from_utf8()  # Convert PoolByteArray to String
 		var response = JSON.parse(body_string)
 		var title = get_node("Label")
 		var desc = get_node("Label2")
+		#startncheck()
 		if response.error == OK:
 			var stage = response.result
 			var ytt = stage['title']
@@ -200,3 +277,7 @@ func _on_request_completed(result, response_code, headers, body):
 		print("Error: Unable to fetch progress (%d)" % response_code)
 
 
+func _on_timeout():
+	# Handle the timeout
+	print("Request timed out after a couple of seconds")
+	startncheck()
